5d4627ac59e5ca295b1116508acc839704cd5476
Lekan Ikotun <lekan.ikotun@aexp.com>
2021-11-11 14:05:32 -0500

    combine networking layer

--- lib/bucket.ts
+++ lib/bucket.ts
@@ -1,13 +1,16 @@
 import * as cdk from '@aws-cdk/core';
 import {Bucket, BucketProps} from "@aws-cdk/aws-s3";
 
+const defaultProps = {
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+}
+
 class S3Bucket extends Bucket {
     constructor(scope: cdk.Construct, id: string, props?: BucketProps) {
         super(scope, id);
 
         new Bucket(this, id, {
-            // default props
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            ...defaultProps,
             ...props
         });
     }
--- lib/networking.ts
+++ lib/networking.ts
@@ -1,29 +1,27 @@
 import * as cdk from '@aws-cdk/core';
-import { Vpc, SubnetType, IVpc, VpcProps } from "@aws-cdk/aws-ec2";
+import { Vpc, SubnetType, VpcProps } from "@aws-cdk/aws-ec2";
+
+const defaultProps = {
+    cidr: "10.0.0.0/16",
+    subnetConfiguration: [
+        {
+            cidrMask: 24,
+            name: 'Public',
+            subnetType: SubnetType.PUBLIC,
+        },
+        {
+            cidrMask: 24,
+            name: 'Private',
+            subnetType: SubnetType.PRIVATE_WITH_NAT,
+        }
+    ]
+}
 
 class Networking extends cdk.Construct {
-
-    public readonly vpc:IVpc;
     constructor(scope: cdk.Construct, id: string, props?: VpcProps) {
         super(scope, id);
 
-        const defaultProps = {
-            cidr: "10.0.0.0/16",
-            subnetConfiguration: [
-                {
-                    cidrMask: 24,
-                    name: 'Public',
-                    subnetType: SubnetType.PUBLIC,
-                },
-                {
-                    cidrMask: 24,
-                    name: 'Private',
-                    subnetType: SubnetType.PRIVATE_WITH_NAT,
-                }
-            ]
-        }
-
-        this.vpc = new Vpc(this, id, {
+        new Vpc(this, id, {
             ...defaultProps,
             ...props
         });
