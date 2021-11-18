c15d81e854267f225f71c0ade471ce4a8545658b
Lekan Ikotun <lekan.ikotun@aexp.com>
2021-11-18 16:07:34 -0500

    add iam

--- lib/api.ts
+++ lib/api.ts
@@ -2,8 +2,8 @@ import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda-nodejs";
 import { Runtime } from "@aws-cdk/aws-lambda";
 import * as path from "path";
-import * as s3 from "@aws-cdk/aws-s3";
-import * as iam from "@aws-cdk/aws-iam";
+import s3 from "@aws-cdk/aws-s3";
+import iam from "@aws-cdk/aws-iam";
 
 interface DocumentManagementAPIProps {
     documentBucket: s3.IBucket;
