e1dbef7b2398ecba9299917f036c69e73fce88d5
Lekan Ikotun <lekan.ikotun@aexp.com>
2021-11-19 16:58:38 -0500

    add iam

--- lib/api.ts
+++ lib/api.ts
@@ -3,7 +3,7 @@ import * as lambda from "@aws-cdk/aws-lambda-nodejs";
 import { Runtime } from "@aws-cdk/aws-lambda";
 import * as path from "path";
 import s3 from "@aws-cdk/aws-s3";
-import iam from "@aws-cdk/aws-iam";
+// import iam from "@aws-cdk/aws-iam";
 
 interface DocumentManagementAPIProps {
     documentBucket: s3.IBucket;
@@ -26,15 +26,15 @@ class DocumentManagementAP extends cdk.Construct {
             }
         });
 
-        const bucketPermissions = new iam.PolicyStatement();
-        bucketPermissions.addResources(`${props.documentBucket.bucketArn}/*`);
+        /*const bucketPermissions = new iam.PolicyStatement();
+        bucketPermissions.addResources(`${props.documentBucket.bucketArn}/!*`);
         bucketPermissions.addActions("s3:GetObject", "s3.PutObject");
         getDocumentsFunction.addToRolePolicy((bucketPermissions));
 
         const bucketContainerPermissions = new iam.PolicyStatement();
         bucketContainerPermissions.addResources(props.documentBucket.bucketArn);
         bucketContainerPermissions.addActions("s3:ListBucket");
-        getDocumentsFunction.addToRolePolicy((bucketContainerPermissions));
+        getDocumentsFunction.addToRolePolicy((bucketContainerPermissions));*/
     }
 }
 
